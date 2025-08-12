e4dae5fd5218fe18dc2f0268fed20a5d749afd40
Cody Marsengill <cod.e.codes.dev@gmail.com>
2025-08-12 03:02:30 -0400

    Update tab.go

--- internal/models/tab.go
+++ internal/models/tab.go
@@ -2,7 +2,6 @@
 package models
 
 import (
-	// "encoding/json"
 	"time"
 )
 
@@ -16,4 +15,56 @@ type Tab struct {
 	TimeSignature string    `json:"time_signature" db:"time_signature"`
 	CreatedAt     time.Time `json:"created_at" db:"created_at"`
 	UpdatedAt     time.Time `json:"updated_at" db:"updated_at"`
+}
+
+func NewEmptyTab(name string) *Tab {
+	emptyLine := "----------------"
+	return &Tab{
+		Name:          name,
+		Artist:        "",
+		Content:       [6]string{emptyLine, emptyLine, emptyLine, emptyLine, emptyLine, emptyLine},
+		Tuning:        [6]string{"e", "B", "G", "D", "A", "E"},
+		Tempo:         120,
+		TimeSignature: "4/4",
+		CreatedAt:     time.Now(),
+		UpdatedAt:     time.Now(),
+	}
+}
+
+type Position struct {
+	String   int
+	Position int
+}
+
+type ViewMode int
+
+const (
+	ViewHome ViewMode = iota
+	ViewEditor
+	ViewBrowser
+	ViewSettings
+	ViewHelp
+)
+
+type EditMode int
+
+const (
+	EditNormal EditMode = iota
+	EditInsert
+	EditSelect
+)
+
+type PlaybackState struct {
+	IsPlaying   bool
+	Position    int
+	Highlighted []Position
+	Tempo       int
+}
+
+type SessionState struct {
+	CurrentTab    *Tab
+	CursorPos     Position
+	ViewMode      ViewMode
+	PlaybackState PlaybackState
+	EditMode      EditMode
 }
